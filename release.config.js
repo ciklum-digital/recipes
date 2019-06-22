57e490526faa42c48dead8ddbd0bd5617189e7e0
arturovt <arthurandrosovich@gmail.com>
2019-06-22 20:49:52 +0300

    feat: switch to yarn and add PR template

--- release.config.js
+++ release.config.js
@@ -1,35 +1,32 @@
 module.exports = {
-    prepare: [
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-                changelogTitle: 'Recipes changelog documentation ✏️',
-            },
-        ],
-        '@semantic-release/npm',
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'package.json',
-                    'package-lock.json',
-                    'CHANGELOG.md',
-                ],
-                // eslint-disable-next-line no-template-curly-in-string
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-            },
-        ],
+  prepare: [
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+        changelogTitle: 'Recipes changelog documentation ✏️'
+      }
     ],
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/npm',
-            {
-                npmPublish: false,
-            },
-        ],
-        '@semantic-release/github',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        // eslint-disable-next-line no-template-curly-in-string
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ]
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+      }
     ],
-}
+    '@semantic-release/github'
+  ]
+};
